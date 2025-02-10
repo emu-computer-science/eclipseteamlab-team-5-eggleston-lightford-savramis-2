10d4ca55fbfc5f8c04bd0392e76898e17d12e216
EricWill082902 <eeggles3@emich.edu>
2025-02-10 12:48:42 -0500

    Added Sad class

--- src/HelloEclipseProject.java
+++ src/HelloEclipseProject.java
@@ -6,6 +6,7 @@ public class Driver {
 		
 		veryHungry();
 		veryGlad();
+		verySad();
 	}
 
 }
---
+++ src/Sad.java
@@ -0,0 +1,9 @@
+
+public class Sad {
+
+	public static void verySad() {
+		// TODO Auto-generated method stub
+		System.out.println("We are all sad");
+	}
+
+}
